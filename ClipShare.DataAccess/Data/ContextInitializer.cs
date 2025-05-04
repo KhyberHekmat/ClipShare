c3ebeb9bb7f2078291f3db1a9c9058a742f1b7d3
KhyberHekmat <hekmatkhyber@outlook.com>
2025-05-04 20:44:44 +0200

    Section 03, Login and Register user in place

--- ClipShare.DataAccess/Data/ContextInitializer.cs
+++ ClipShare.DataAccess/Data/ContextInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ClipShare.Core.Entities;
+using ClipShare.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +12,52 @@ namespace ClipShare.DataAccess.Data
 {
     public static class ContextInitializer
     {
-        public static void Initialize(Context context)
+        public static async Task InitializeAsync(Context context,
+            UserManager<AppUser> userManager,
+            RoleManager<AppRole> roleManager)
         {
             if(context.Database.GetPendingMigrations().Count() > 0)
             {
                 context.Database.Migrate();
+            }
+
+            if (!roleManager.Roles.Any())
+            {
+                foreach(var role in SD.Roles)
+                {
+                    await roleManager.CreateAsync(new AppRole { Name = role });
+                }
+            }
+
+            if (!userManager.Users.Any())
+            {
+                var admin = new AppUser
+                {
+                    Name = "Admin",
+                    Email="admin@admin.com",
+                    UserName="admin"
+                };
+                await userManager.CreateAsync(admin, "Password123");
+                await userManager.AddToRolesAsync(admin, [SD.AdminRole, SD.UserRole, SD.ModeratorRole]);
+
+                var john = new AppUser
+                {
+                    Name = "John",
+                    Email = "john@admin.com",
+                    UserName = "john"
+                };
+                await userManager.CreateAsync(john, "Password123");
+                await userManager.AddToRoleAsync(john, SD.UserRole);
+
+                var mary = new AppUser
+                {
+                    Name = "Mary",
+                    Email = "mary@admin.com",
+                    UserName = "mary"
+                };
+                await userManager.CreateAsync(mary, "Password123");
+                await userManager.AddToRoleAsync(mary, SD.ModeratorRole);
+
             }
         }
     }
--- ClipShare/Program.cs
+++ ClipShare/Program.cs
@@ -1,16 +1,21 @@
+using ClipShare.Core.Entities;
 using ClipShare.DataAccess.Data;
 using ClipShare.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+//These settings are comming from WebApplicationExtensions class
 builder.AddApplicationServices();
+builder.AddAuthenticationServices();
 
 var app = builder.Build();
 
@@ -26,17 +31,18 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+// we are going to use the Authentication before Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-InitializContext();
+await InitializContextAsync();
 app.Run();
 
-void InitializContext()
+async Task InitializContextAsync()
 {
     using var scope = app.Services.CreateScope();
     var services = scope.ServiceProvider;
@@ -44,7 +50,10 @@ void InitializContext()
     try
     {
         var context = scope.ServiceProvider.GetService<Context>();
-        ContextInitializer.Initialize(context);
+        var userManager = scope.ServiceProvider.GetService<UserManager<AppUser>>();
+        var roleManager = scope.ServiceProvider.GetService<RoleManager<AppRole>>();
+
+        await ContextInitializer.InitializeAsync(context,userManager,roleManager);
     }
     catch(Exception ex)
     {
